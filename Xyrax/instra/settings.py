8b294a14c2b9e9db3ce77bd70b4b0fe4b8db0ab0
Riya <sdeb14354@gmail.com>
2026-02-25 03:34:23 +0530

    Add proper AppConfig for analytics

--- Xyrax/instra/settings.py
+++ Xyrax/instra/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-instra-change-this-in-production-abc123xyz'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
